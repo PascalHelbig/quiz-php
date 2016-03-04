7522ea37e866b80b6116e01725647bf5c01997c5
Pascal Helbig <pascalhelbig@gmail.com>
2016-03-04 19:12:53 +0100

    connection to psql & GET /questions

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
+// Return jsons:
+header('Content-Type: application/json');
 require_once __DIR__ . '/vendor/autoload.php';
 
 $klein = new Klein\Klein();
 
+// connect to db:
+$klein->respond(function ($request, $response, $service, $app) use ($klein) {
+    $app->register('db', function () {
+        return new PDO('pgsql:host=lamp.wlan.hwr-berlin.de;port=5432;dbname=csdb3;user=csdb3;password=csdb3');
+    });
+});
+
 $klein->respond('GET', '/', function () {
     return "Hello World!";
+});
+
+$klein->respond('GET', '/questions', function ($request, $response, $service, $app) {
+    $sql = 'SELECT * FROM quiz';
+    return json_encode($app->db->query($sql)->fetchAll(PDO::FETCH_ASSOC));
 });
 
 $klein->dispatch();
