11a52043800f95532bef9358263a13f04fcdf235
Pascal Helbig <pascalhelbig@gmail.com>
2016-03-21 10:48:59 +0100

    PHP vereinfacht :metal:

--- index.php
+++ index.php
@@ -1,29 +1,19 @@
 <?php
 // Return jsons:
 header('Content-Type: application/json');
-require_once __DIR__ . '/vendor/autoload.php';
 
-$klein = new Klein\Klein();
+$pdo = new PDO('pgsql:host=lamp.wlan.hwr-berlin.de;port=5432;dbname=csdb3;user=csdb3;password=csdb3');
 
-// connect to db:
-$klein->respond(function ($request, $response, $service, $app) use ($klein) {
-    $app->register('db', function () {
-        return new PDO('pgsql:host=lamp.wlan.hwr-berlin.de;port=5432;dbname=csdb3;user=csdb3;password=csdb3');
-    });
-});
-
-$klein->respond('GET', '/', function () {
-    return "Hello World!";
-});
-
-$klein->respond('GET', '/questions', function ($request, $response, $service, $app) {
+if (isset($_GET['questions'])) {
     $sql = 'SELECT * FROM quiz';
-    return json_encode($app->db->query($sql)->fetchAll(PDO::FETCH_ASSOC));
-});
+    echo json_encode($pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC));
+    return;
+}
 
-$klein->respond('GET', '/categories', function ($request, $response, $service, $app) {
+if (isset($_GET['categories'])) {
     $sql = 'SELECT * FROM category';
-    return json_encode($app->db->query($sql)->fetchAll(PDO::FETCH_ASSOC));
-});
+    echo json_encode($pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC));
+    return;
+}
 
-$klein->dispatch();
+echo 'Hello World!';
